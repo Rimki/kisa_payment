61071c232dc9d559a9f4475307e6ad9cdc2126b1
Rimki <yhs693@naver.com>
2019-03-08 12:40:46 +0900

    day04

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var path = require('path');
 app.use(express.urlencoded());
 app.use(express.json());
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connectionPool = mysql.createPool({
+  connectionLimit : 5,
   host     : 'localhost',
   user     : 'root',
   password : 'k1085317',
@@ -20,14 +21,16 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/index',function(req,res){
+app.get('/home',function(req,res){
     res.render('home');
 })
 
 app.get('/signup',function(req,res){
     res.render('signup');
 })
-
+app.get('/login',function(req,res){
+    res.render('login');
+})
 
 app.get('/authResult',function(req,res){
    var auth_code = req.query.code;
@@ -66,8 +69,9 @@ app.post('/join', function(req, res){
     var accessToken =req.body.accessToken;
     var refreshToken = req.body.refreshToken;
     var userseqnum = req.body.userseqnum;
-    connection.connect();
-    connection.query('INSERT into user(userid,username,userpassword,accessToken,refreshToken,userseqnum) values(?,?,?,?,?,?) ',[id,name,password,accessToken,refreshToken,userseqnum], function (error, results, fields) {
+    connectionPool.getConnection(function(err,conn){
+
+    conn.query('INSERT into user(userid,username,userpassword,accessToken,refreshToken,userseqnum) values(?,?,?,?,?,?) ',[id,name,password,accessToken,refreshToken,userseqnum], function (error, results, fields) {
     if (error){ 
         throw error;
     }
@@ -76,12 +80,33 @@ app.post('/join', function(req, res){
         
     }
   });
-  connection.end();
+  connectionPool.end();
+})
+});
+app.post('/login',function(req,res){
+    var id =req.body.id;
+    var password = req.body.password;
+    connectionPool.getConnection(function(err,conn){
+        conn.query("select * from user where userid= ?",[id],function(err,result){
+            if(err){
+                throw err;
+            }
+            else{
+                var userData= result;
+                console.log(userData);
+                conn.release();
+                if(userData.userpassword== password){
+                    res.json(userData.accessToken);
+                }
+            }
+        })
+    })
 })
 
-app.get('/user',function(req,res){
-    var accessToken = "191b2bf1-1d87-45d2-a8ae-387b372b814a";
-    var requestURL = "https://testapi.open-platform.or.kr/user/me?user_seq_no=1100034850";
+app.post('/user',function(req,res){
+    var accessToken = req.body.accessToken;
+    var userseqnum= req.body.userseqnum;
+    var requestURL = "https://testapi.open-platform.or.kr/user/me?user_seq_no="+userseqnum;
     var option = {
         method :"GET",
         url : requestURL,
@@ -90,13 +115,15 @@ app.get('/user',function(req,res){
         }
     }
     request(option,function(err,response,body){
-        res.send(body);
+        obj =JSON.parse(body);
+        res.send(obj);
     })
 })
 
 app.get('/balance',function(req,res){
-    var accessToken = "191b2bf1-1d87-45d2-a8ae-387b372b814a";
-    var requestURL = "https://testapi.open-platform.or.kr/v1.0/account/balance?fintech_use_num=199004072057725907018697&tran_dtime=20190307040000";
+    var accessToken = req.body.accessToken;
+    var fintech_use_num = req.body.fintech_use_num;
+    var requestURL = "https://testapi.open-platform.or.kr/v1.0/account/balance?fintech_use_num="+fintech_use_num+"&tran_dtime=20190307040000";
 
     var option = {
         method :"GET",
